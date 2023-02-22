44dfd1007e9945e3b4051f01f43f264ee692bbfd
Matthew Pauly <mjpauly@mac.com>
2023-02-22 14:21:39 -0800

    (unfinished) SceneDelegate+WKWebView wrapper

---
+++ src/app/ios/top/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  storyboardtest
+//
+//  Created by Matthew Pauly on 1/23/23.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        print("loaded view controller")
+        
+        view.addSubview(webView)
+        NSLayoutConstraint.activate([
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
+            webView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor)
+        ])
+        
+        let contentController = self.webView.configuration.userContentController
+        contentController.add(self, name: "toggleMessageHandler")
+        
+        if let url = Bundle.main.url(forResource: "index", withExtension: "html") {
+            webView.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent())
+        }
+    }
+    
+    private lazy var webView: WKWebView = {
+        let webView = WKWebView()
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        return webView
+    }()
+    
+    //added for full screen
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+
+        additionalSafeAreaInsets.bottom -= bottomLayoutGuide.length
+        additionalSafeAreaInsets.top -= topLayoutGuide.length
+    }
+}
+
+extension ViewController: WKScriptMessageHandler{
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        // receive message from webapp
+        guard let dict = message.body as? [String : AnyObject] else {
+            return
+        }
+        print(dict)  // show the dictionary containing the message we received
+        
+        // send the message we received back to the webapp by changing the page's text
+        guard let message = dict["message"] else {
+            return
+        }
+
+        let script = "document.getElementById('value').innerText = \"\(message)\""
+
+        webView.evaluateJavaScript(script) { (result, error) in
+            if let result = result {
+                print("Label is updated with message: \(result)")
+            } else if let error = error {
+                print("An error occurred: \(error)")
+            }
+        }
+    }
+}
